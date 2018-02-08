431477b2b358fb6d8f57d51176d40ad59a8856a3
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-02-08 14:41:12 +0530

    Create addevenRodd.c

---
+++ addevenRodd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int num1,num2,add;
+            scanf("%d%d",&num1,&num2);
+            add=num1+num2;
+            if(add%2==0)
+            printf("Even");
+            else
+	printf("Odd");
+            return 0;
+}
